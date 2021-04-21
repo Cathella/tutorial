691c28e5e90499bef048b7e4d74429bae3b20f3b
Cathella <ncathie214@gmail.com>
2021-04-21 07:41:53 +0300

    using props to access objects

--- src/index.js
+++ src/index.js
@@ -1,40 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// css
 import "./index.css";
+// setup vars
+const firstBook = {
+  img:
+    "https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+  title: "I Love You to the Moon and Back",
+  author: "Amelia Hepworth",
+};
+
+const secondBook = {
+  img:
+    "https://images-na.ssl-images-amazon.com/images/I/51a9yGrI3bL._AC_UL200_SR200,200_.jpg",
+  title: "Why a Daughter Needs a Mom",
+  author: "Susanna Leonard Hill",
+};
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={props.img} alt="" />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   );
 };
-
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
-    alt=""
-  />
-);
-
-const Title = () => <h1>I Love You to the Moon and Back</h1>;
-const Author = () => <h4>Amelia Hepworth</h4>;
 
 ReactDOM.render(<BookList />, document.getElementById("root"));
